741d65ec163cbfbb1c38714910d108344be8bbbd
h3r0TOz3r0 <anna.devries4@outlook.com>
2019-01-29 10:34:23 -0500

    finished setup script, testing now

--- setup.sh
+++ setup.sh
@@ -12,14 +12,51 @@
 
 
 ## exit on failure
-set -eEuo pipefail
-trap 'echo "Aborting (errexit line $LINENO). Exit code: $?" >&2' ERR
-IFS=$'\n\t'
+#set -eEuo pipefail
+#trap 'echo "Aborting (errexit line $LINENO). Exit code: $?" >&2' ERR
+#IFS=$'\n\t'
+
+
+# Update
+sudo apt-get install update
+sudo apt-get upgrade
+sudo apt-get install python3
+sudo apt-get install pythong3-pip
 
 
 ## Install dependencies
+sudo apt-get install git-core
 sudo apt-get install -y python-smbus
 sudo apt-get install -y i2c-tools
+cd
+if [ ! -d "$wiringPi/" ]; then
+	git clone git://git.drogon.net/wiringPi
+	cd ~/wiringPi
+	git pull origin
+	./build					## Troubleshooting: gpio -v
+						## Toubleshooting: gpio readall
+fi
+
+## If using python
+# sudo raspi-config
+# Select Interfacing Options
+# Select SPI, select yes, enter
+# Repeat for I2C and Serial
+if grep -Fxq "alias python='/usr/bin/python3'" ~/.bashrc
+then
+        echo "Python3 Already Configured"
+else
+        echo "alias python='/usr/bin/python3'" >> ~/.bashrc ## >> appends to end
+fi
+
+if grep -Fxq "alias pip=pip3" ~/.bashrc
+then
+        echo "Pip3 Already Configured"
+else
+        echo "alias pip=pip3" >> ~/.bashrc ## >> appends to end
+fi
+
+source ~/.bashrc
 
 
 ## Update /etc/modules
@@ -41,8 +78,21 @@ fi
 ## Update /boot/config.txt
 # Remove old parameters
 sudo sed -i '/dtparam=i2c1=/d' /boot/config.txt
-sudo sed -i '/dtparam=12c_arm=/d' /boot/config.txt
+sudo sed -i '/dtparam=i2c_arm=/d' /boot/config.txt
+sudo sed -i '/dtparam=i2c_vc=/d' /boot/config.txt
+
 
 # Add new parameters\
 sudo bash -c 'echo "dtparam=i2c1=on" >> /boot/config.txt'
 sudo bash -c 'echo "dtparam=i2c_arm=on" >> /boot/config.txt'
+sudo bash -c 'echo "dtparam=i2c_vc=on" >> /boot/config.txt'
+
+
+sudo reboot
+
+## Troubleshooting
+# after rebooting...
+# ls /dev/*spi* should return /dev/spidev0.0 /dev/spidev0.1
+# ls /dev/*i2c* should return /dev/i2c-1
+# sudo i2cdetect -y 1
+# sudo i2cget
