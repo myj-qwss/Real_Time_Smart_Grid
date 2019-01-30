2b484fe85bf2f727730e88a0d5dc1b92ecbe762f
h3r0TOz3r0 <anna.devries4@outlook.com>
2019-01-30 13:44:43 -0500

    working script

--- magnet_trial.py
+++ magnet_trial.py
@@ -1,40 +1,38 @@
 #!/usr/bin/env python2.7
 
-#import MySQLdb
 from smbus import SMBus
 import time
 
-#db = MYSQLdb.connect("localhost", "z3r0", "toor", "smbtable")
-#curs = db.cursor()
-
 bus = SMBus(1)
-
 addr = 0x0b
 
 t_end = time.time() + 60 * 15
 
 while time.time() < t_end:
-	voltage = bus.read_byte_data(addr, 0x09)
-	current = bus.read_byte_data(addr, 0x0a)
-	temperature = bus.read_byte_data(addr, 0x08)
-	relsoc = bus.read_byte_data(addr, 0x0d)
+	voltage = bus.read_word_data(addr, 0x09)
+	current = bus.read_word_data(addr, 0x0a)
+	#temperature = bus.read_word_data(addr, 0x08)
+	relsoc = bus.read_word_data(addr, 0x0d)
 	adssoc = bus.read_byte_data(addr, 0x0e)
 
-	print "Voltage: "
-	print voltage
-	print "Current: "
-	print current
-	print "Temperature: "
-	print temperature
-	print "Relsoc: "
-	print relsoc
-	print "Adssoc: "
-	print adssoc
-#	myquery = "INSERT INTO smtable (time, temperature, voltage, current, relsoc, absoc) VALUES ('" + time() + "','" + temperature + "','" + voltage + "','" + current + "','" + relsoc + "','" + absoc + "')"
-	#curs.execute(myquery)
-	#db.comit()
+	#voltage = voltage / (2^16)
+	#current = current / (2^16)
 	
-#	print(myquery)
+	print("Voltage: ")
+	print(voltage)
+	print("Current: ")
+	print(current)
+	print("Relsoc: ")
+	print(relsoc)
+	print("Adssoc: ")
+	print(adssoc)
 
-#report = "SELECT time, temperature, voltage, current, relsoc, absoc FROM smbtable"
-#cursor.execute(report)
+#	#voltage = int(voltage) / 65536
+#	current = current / 65536
+#	print("New Voltage: ")
+#	print(voltage)
+#	print("New Current: ")
+#	print(current)
+
+	time.sleep(1)
+
